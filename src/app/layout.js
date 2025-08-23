99caecd9568a2480cc09b64eaf25e3e0cb30f3ba
Your Name <nazrulislam.cse28@gmail.com>
2025-08-24 00:46:49 +0600

    Navabr added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/shared/Navbar";
+import NavHeader from "@/components/shared/NavHeader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavHeader/>
+        <Navbar/>
         {children}
       </body>
     </html>
