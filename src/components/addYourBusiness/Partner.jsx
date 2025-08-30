10d1a2155996445f7971a2acb9bf671252298a4b
Your Name <nazrulislam.cse28@gmail.com>
2025-08-31 01:35:13 +0600

    Click here to submit your offer button added

--- src/components/addYourBusiness/Partner.jsx
+++ src/components/addYourBusiness/Partner.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 const Partner = () => {
   return (
     <div>
       <div className='w-7xl mx-auto mt-16'>
             <h1 className='text-center font-bold text-5xl common-text montserrat-text '>Why Partner With Us</h1>
-            <h3 className='text-center mt-6'>A trusted platform built for high-impact results. <br /> With over a million verified members and thousands of trusted partners, we help your business <br /> stand out. Whether you're a local shop or a national brand, your offers gain powerful exposure <br /> across our app, website, and marketing channels — without the stress of setup fees or long <br /> commitments. You bring the offer, we bring the audience.</h3>
+            <h3 className='text-center mt-6 text-lg'>A trusted platform built for high-impact results. <br /> With over a million verified members and thousands of trusted partners, we help your business <br /> stand out. Whether you're a local shop or a national brand, your offers gain powerful exposure <br /> across our app, website, and marketing channels — without the stress of setup fees or long <br /> commitments. You bring the offer, we bring the audience.</h3>
+            <Button className='common-bg'>Click Here To Submit Your Offer</Button>
       </div>
     </div>
   )
