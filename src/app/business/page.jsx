8015cf3addd13affffa261f0136a4f37191c6bdb
Your Name <nazrulislam.cse28@gmail.com>
2025-09-02 02:27:56 +0600

    FAQ section added in the add your business page

--- src/app/business/page.jsx
+++ src/app/business/page.jsx
@@ -1,8 +1,10 @@
 
+import FAQ from '@/components/addYourBusiness/FAQ'
 import Header from '@/components/addYourBusiness/Header'
 import Partner from '@/components/addYourBusiness/Partner'
 import WhatYouGet from '@/components/addYourBusiness/WhatYouGet'
 import Works from '@/components/addYourBusiness/Works'
+import Faq from '@/components/LandingPage/Faq'
 import Subscription from '@/components/LandingPage/Subscription'
 import React from 'react'
 
@@ -14,6 +16,7 @@ const BusinessPage = () => {
       <Works/>
       <Subscription/>
       <WhatYouGet/>
+      <FAQ/>
     </div>
   )
 }
