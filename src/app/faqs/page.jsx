b36d13a3f52c170c8fb97f168ea7fd2aa59680cf
Your Name <nazrulislam.cse28@gmail.com>
2025-09-04 03:45:30 +0600

    Project updated

--- src/app/faqs/page.jsx
+++ src/app/faqs/page.jsx
@@ -1,12 +1,11 @@
 
-import Register6 from '@/components/authentication/Register6'
+
 import React from 'react'
 
 const FAQSPage = () => {
   return (
     <div>
       <h1>This is FAQ Page</h1>
-      <Register6/>
     </div>
   )
 }
